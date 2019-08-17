5b1ab385f259fd1b3638375318af06d40d93c352
Ethan Merrill <eth22mer@gmail.com>
2019-08-17 09:12:13 -0400

    Successfully writes to sheet

--- Sheet_Writer.py
+++ Sheet_Writer.py
@@ -8,66 +8,49 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import keys
 from pprint import pprint
-
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = keys.SheetId
-SAMPLE_RANGE_NAME = 'A1:B5'
+import datetime
 
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    #setup
+    #The starting row
+    i=2
+    #infinite loop
+    while True:
+        studentID = input("swipe StudentID")
+        #parse the student ID from the raw swiped data
 
-    service = build('sheets', 'v4', credentials=creds)
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
+        currentDT = datetime.datetime.now()
 
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s' % (row[0]))
-"""
-    writeValues= [[u'Zellea1', u'Zelleb1'],]
-    writeRange = "Sheet1!A15:C"
+        rowData = [str(currentDT), studentID]
+        insert_New_Name (rowData,i)
+        i = i+1
+
+def insert_New_Name(rowDataArr,i):
+    #writeValues= [[u'Zellea1', u'Zelleb1'],]
+    writeValues= [[rowDataArr[0], rowDataArr[1]],]
+    writeRange = "Sheet1!A"+str(i)+":B"
     sheet_Write(keys.SheetId, writeValues, writeRange)
+    #write to a local file for redundancy
+    #check to see if file exists, if not make one
+
+    #write to file
 
 def sheet_Write(spreadsheet_ID, rowEntryArr, location):
+    #call the token.pickle file which was generated using the quickstart code and is the api key
     with open('token.pickle', 'rb') as token:
        creds = pickle.load(token)
+    #open and setup the api 
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
 
+    #what will be pasted into the sheet
     myBody = {u'range': location, u'values': rowEntryArr, u'majorDimension': u'ROWS'}
     rangeOutput = location
     response = sheet.values().update( spreadsheetId=spreadsheet_ID, range=rangeOutput, valueInputOption='RAW', body=myBody ).execute()
 
+
 if __name__ == '__main__':
     main()
+
+
